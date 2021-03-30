93e4bbb6cd5cfae911d865d6d41ed9fa56e9d885
千风 <qianfeng.zqf@alibaba-inc.com>
2021-03-30 11:31:37 +0800

    perf: add error info
    add solution info when version of pulumi cli is too old

--- test/main.ts
+++
@@ -1,7 +0,0 @@
-import * as shell from 'shelljs';
-
-export function getLatestVersionOfPackage(pkgName) {
-  const version = shell.exec(`npm show ${pkgName} version`, {silent:true}).stdout;
-  // 去掉第二行的空行
-  return version.split('\n')[0];
-}
